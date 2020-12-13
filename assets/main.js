c75fd6e42bc158165678f9bb0df8b619ea4548a8
PodZ <74189166+podzit@users.noreply.github.com>
2020-12-13 23:25:25 +0100

    Update main.js
    random sur catégorie pour classer les images dans des dossiers séparés 1 et 2 dans assets/img et faciliter l'ajout de nouvelles cartes.

--- assets/main.js
+++ assets/main.js
@@ -8,26 +8,56 @@
             
             /* Mise à zéro des variables */
 
-            var random = undefined;
-            var random2 = undefined;
-            var valeur1 = undefined;
-            var valeur2 = undefined;
             var cat = undefined;
             var cat2 = undefined;
+            var choix = undefined;
+            var choix2 = undefined;
+            var valeur1 = undefined;
+            var valeur2 = undefined;
             var result = undefined;
+            var min = undefined;
+            var max = undefined;
+            var max2 = undefined;
+            var max3 = undefined;
 
-            /* Tirage aléatoire des cartes*/
 
-            var min=1; 
-            var max=24;  
-            random = Math.floor(Math.random() * (max - min)) + min;
-            random2 = Math.floor(Math.random() * (max - min)) + min;
+            /* Nombre de cartes dans les dossier 1 et 2 */
+
+            var max2 = 11 + 1;
+            var max3 = 12 + 1;
+
+            /* Tirage catégorie */
+            
+            min = 1; 
+            max = 3;  
+            cat = Math.floor(Math.random() * (max - min)) + min;
+            cat2 = Math.floor(Math.random() * (max - min)) + min;
+
+            /* Tirage cartes */
+            
+            if (cat == 1){
+            choix = Math.floor(Math.random() * (max2 - min)) + min;
+            }
+            
+            if(cat == 2){
+            choix = Math.floor(Math.random() * (max2 - min)) + min;
+            }
+
+            if (cat2 == 1){
+            choix2 = Math.floor(Math.random() * (max3 - min)) + min;
+            }
+
+            if(cat2 == 2){
+            choix2 = Math.floor(Math.random() * (max3 - min)) + min;
+            }
 
             /* Construction nom de la carte en num.jpeg*/
 
             var extension = '.jpeg';
-            var image = (random)+(extension);
-            var image2 = (random2)+(extension);
+            var sep = '/';
+
+            var image = cat + sep + choix + extension;
+            var image2 = cat2 + sep + choix2 + extension;
 
             /* Affichage en HTML */
             var code1 = "\<img src\=\"assets\/img\/";
@@ -42,93 +72,80 @@
 
             /* Assignation des valeurs selon les cartes */
 
-            if (random == 1){valeur1 = 1};
-            if (random == 2){valeur1 = 2};
-            if (random == 3){valeur1 = 2};
-            if (random == 4){valeur1 = 3};
-            if (random == 5){valeur1 = 3};
-            if (random == 6){valeur1 = 4};
-            if (random == 7){valeur1 = 4};
-            if (random == 8){valeur1 = 4};
-            if (random == 9){valeur1 = 5};
-            if (random == 10){valeur1 = 5};
-            if (random == 11){valeur1 = 6};
-            if (random == 12){valeur1 = 1};
-            if (random == 13){valeur1 = 1};
-            if (random == 14){valeur1 = 2};
-            if (random == 15){valeur1 = 2};
-            if (random == 16){valeur1 = 3};
-            if (random == 17){valeur1 = 3};
-            if (random == 18){valeur1 = 4};
-            if (random == 19){valeur1 = 4};
-            if (random == 20){valeur1 = 4};
-            if (random == 21){valeur1 = 5};
-            if (random == 22){valeur1 = 5};
-            if (random == 23){valeur1 = 6};
+            if (cat == 1 && choix == 1){valeur1 = 1};
+            if (cat == 1 && choix == 2){valeur1 = 2};
+            if (cat == 1 && choix == 3){valeur1 = 2};
+            if (cat == 1 && choix == 4){valeur1 = 3};
+            if (cat == 1 && choix == 5){valeur1 = 3};
+            if (cat == 1 && choix == 6){valeur1 = 4};
+            if (cat == 1 && choix == 7){valeur1 = 4};
+            if (cat == 1 && choix == 8){valeur1 = 4};
+            if (cat == 1 && choix == 9){valeur1 = 5};
+            if (cat == 1 && choix == 10){valeur1 = 5};
+            if (cat == 1 && choix == 11){valeur1 = 6};
+            if (cat == 2 && choix == 1){valeur1 = 1};
+            if (cat == 2 && choix == 2){valeur1 = 1};
+            if (cat == 2 && choix == 3){valeur1 = 2};
+            if (cat == 2 && choix == 4){valeur1 = 2};
+            if (cat == 2 && choix == 5){valeur1 = 3};
+            if (cat == 2 && choix == 6){valeur1 = 3};
+            if (cat == 2 && choix == 7){valeur1 = 4};
+            if (cat == 2 && choix == 8){valeur1 = 4};
+            if (cat == 2 && choix == 9){valeur1 = 4};
+            if (cat == 2 && choix == 10){valeur1 = 5};
+            if (cat == 2 && choix == 11){valeur1 = 5};
+            if (cat == 2 && choix == 12){valeur1 = 6};
 
-            if (random2 == 1){valeur2 = 1};
-            if (random2 == 2){valeur2 = 2};
-            if (random2 == 3){valeur2 = 2};
-            if (random2 == 4){valeur2 = 3};
-            if (random2 == 5){valeur2 = 3};
-            if (random2 == 6){valeur2 = 4};
-            if (random2 == 7){valeur2 = 4};
-            if (random2 == 8){valeur2 = 4};
-            if (random2 == 9){valeur2 = 5};
-            if (random2 == 10){valeur2 = 5};
-            if (random2 == 11){valeur2 = 6};
-            if (random2 == 12){valeur2 = 1};
-            if (random2 == 13){valeur2 = 1};
-            if (random2 == 14){valeur2 = 2};
-            if (random2 == 15){valeur2 = 2};
-            if (random2 == 16){valeur2 = 3};
-            if (random2 == 17){valeur2 = 3};
-            if (random2 == 18){valeur2 = 4};
-            if (random2 == 19){valeur2 = 4};
-            if (random2 == 20){valeur2 = 4};
-            if (random2 == 21){valeur2 = 5};
-            if (random2 == 22){valeur2 = 5};
-            if (random2 == 23){valeur2 = 6};
+            if (cat2 == 1 && choix2 == 1){valeur2 = 1};
+            if (cat2 == 1 && choix2 == 2){valeur2 = 2};
+            if (cat2 == 1 && choix2 == 3){valeur2 = 2};
+            if (cat2 == 1 && choix2 == 4){valeur2 = 3};
+            if (cat2 == 1 && choix2 == 5){valeur2 = 3};
+            if (cat2 == 1 && choix2 == 6){valeur2 = 4};
+            if (cat2 == 1 && choix2 == 7){valeur2 = 4};
+            if (cat2 == 1 && choix2 == 8){valeur2 = 4};
+            if (cat2 == 1 && choix2 == 9){valeur2 = 5};
+            if (cat2 == 1 && choix2 == 10){valeur2 = 5};
+            if (cat2 == 1 && choix2 == 11){valeur2 = 6};
+            if (cat2 == 2 && choix2 == 1){valeur2 = 1};
+            if (cat2 == 2 && choix2 == 2){valeur2 = 1};
+            if (cat2 == 2 && choix2 == 3){valeur2 = 2};
+            if (cat2 == 2 && choix2 == 4){valeur2 = 2};
+            if (cat2 == 2 && choix2 == 5){valeur2 = 3};
+            if (cat2 == 2 && choix2 == 6){valeur2 = 3};
+            if (cat2 == 2 && choix2 == 7){valeur2 = 4};
+            if (cat2 == 2 && choix2 == 8){valeur2 = 4};
+            if (cat2 == 2 && choix2 == 9){valeur2 = 4};
+            if (cat2 == 2 && choix2 == 10){valeur2 = 5};
+            if (cat2 == 2 && choix2 == 11){valeur2 = 5};
+            if (cat2 == 2 && choix2 == 12){valeur2 = 6};
 
-            /* Attribution à l'une ou l'autre des catégories en fonction du numéro de carte (cat1 = complot, cat2 = complotistes) */
-            if (random < 12){
-                    cat = 1;
-                }
-            if(random > 11){
-                    cat = 2;
-                }
-            if (random2 < 12){
-                    cat2 = 1;
-                }
-            if(random2 > 11){
-                    cat2 = 2;
-                }
-            
+           
             /* Affichage du résultats en fonction des cas */
 
             if (valeur1 > valeur2 && cat == 1 && cat2 == 2) {
                     result = "Bravo \! Tu as fais gagn\351 le nouvel ordre mondial";
                 } 
             else if (valeur1 > valeur2 && cat == 1 && cat2 == 1) {
-                    result = "Bravo \! Tu as tu\351 tes alli\351s";
+                    result = "Wahou \! Tu as tu\351 tes alli\351s";
                 }
             else if (valeur1 > valeur2 && cat == 2 && cat2 == 1){
-                    result = "Bravo \! Tu as vaincu l\'\351lite p\351dophile satanique";
+                    result = "Yes \! Tu as vaincu l\'\351lite p\351dophile satanique";
                 }
             else if (valeur1 > valeur2 && cat == 2 && cat2 == 2){
-                    result = "Bravo \! Tu es le survivant de ta guilde";
+                    result = "Wahou \! Tu es le survivant de ta guilde";
                 }
             else if (valeur2 > valeur1 && cat2 == 1 && cat == 2){
                     result = "Looser \! Le complot mondial a eu ta peau"; 
                 }
             else if (valeur2 > valeur1 && cat2 == 1 && cat == 1){
-                    result = "Looser \! Trop de complot tue le complot"; 
+                    result = "Oups \! Trop de complot tue le complot"; 
                 }
             else if (valeur2 > valeur1 && cat2 == 2 && cat == 1){
                     result = "Looser \! Tu as perdu contre la guilde de la chloroquine";
             }
             else if (valeur2 > valeur1 && cat2 == 2 && cat == 2){
-                    result = "Looser \! Tu as \351t\351 trahis par tes amis";
+                    result = "Dommage \! Tu as \351t\351 trahis par tes amis";
             }
             else{
                 result = "Match nul\: Personne n\'est sorti vivant de ce duel";
@@ -136,7 +153,7 @@
 
             /* débug */
             
-            /* console.log(random,random2,valeur1,valeur2,cat,cat2); */
+            /*console.log(valeur1,valeur2,cat,cat2,choix,choix2);*/
 
             
             /* Centre l'affichage des résultats */
