cbe9ea09a3c08241df5ace7f3066aaaca7228b13
PodZ <74189166+podzit@users.noreply.github.com>
2020-12-14 18:37:41 +0100

    Update main.js
    ajout de l'élément perso dans le deck pour personnaliser la réponse et aider à la compréhension des cartes.

--- assets/main.js
+++ assets/main.js
@@ -10,117 +10,140 @@ const deck = [
   {
     categorie: 'comploteurs',
     force: 1,
-    nom: 1 // <- la carte 1.jpeg
+    nom: 1, // <- la carte 1.jpeg
+    perso: 'Sibeth Ndiaye' 
   },
   {
     categorie: 'comploteurs',
     force: 2,
-    nom: 2
+    nom: 2,
+    perso: 'Jean Castex'
   },
   {
     categorie: 'comploteurs',
     force: 2,
-    nom: 3
+    nom: 3,
+    perso: 'Le masque'
   },
   {
     categorie: 'comploteurs',
     force: 3,
-    nom: 4
+    nom: 4,
+    perso: 'Big Pharma'
   },
   {
     categorie: 'comploteurs',
     force: 3,
-    nom: 5
+    nom: 5,
+    perso: 'George Soros'
   },
   {
     categorie: 'comploteurs',
     force: 4,
-    nom: 6
+    nom: 6,
+    perso: 'La 5G'
   },
   {
     categorie: 'comploteurs',
     force: 4,
-    nom: 7
+    nom: 7,
+    perso: 'Bill Gates'
   },
   {
     categorie: 'comploteurs',
     force: 4,
-    nom: 8
+    nom: 8,
+    perso: 'BFM TV'
   },
   {
     categorie: 'comploteurs',
     force: 5,
-    nom: 9
+    nom: 9,
+    perso: 'Le vaccin'
   },
   {
     categorie: 'comploteurs',
     force: 5,
-    nom: 10
+    nom: 10,
+    perso: 'Les illuminatis'
   },
   {
     categorie: 'comploteurs',
     force: 6,
-    nom: 11
+    nom: 11,
+    perso: 'Les franc-maçons'
   },
   {
     categorie: 'complotistes',
     force: 1,
-    nom: 1
+    nom: 1,
+    perso: 'Hold Up'
   },
   {
     categorie: 'complotistes',
     force: 1,
-    nom: 2
+    nom: 2,
+    perso: 'CNews'
   },
   {
     categorie: 'complotistes',
     force: 2,
-    nom: 3
+    nom: 3,
+    perso: 'Donald Trump'
   },
   {
     categorie: 'complotistes',
     force: 2,
-    nom: 4
+    nom: 4,
+    perso: 'Pascal Praud'
   },
   {
     categorie: 'complotistes',
     force: 3,
-    nom: 5
+    nom: 5,
+    perso: 'Q'
   },
   {
     categorie: 'complotistes',
     force: 3,
-    nom: 6
+    nom: 6,
+    perso: 'Christian Perronne'
   },
   {
     categorie: 'complotistes',
     force: 4,
-    nom: 7
+    nom: 7,
+    perso: 'Ema Krusi'
   },
   {
     categorie: 'complotistes',
     force: 4,
-    nom: 8
+    nom: 8,
+    perso: 'Eve Engerer'
   },
   {
     categorie: 'complotistes',
     force: 4,
-    nom: 9
+    nom: 9,
+    perso: 'Eric Zemmour'
   },
   {
     categorie: 'complotistes',
     force: 5,
-    nom: 10
+    nom: 10,
+    perso: 'Didier Raoult'
   },
   {
     categorie: 'complotistes',
     force: 5,
-    nom: 11
+    nom: 11,
+    perso: 'Hydroxychloroquine'
   },
   {
     categorie: 'complotistes',
     force: 6,
-    nom: 12
+    nom: 12,
+    perso: 'Sylvano Trotta'
   },
 ]
 
@@ -135,31 +158,31 @@ document.getElementById("img-ennemi").innerHTML = `<img src="assets/img/${carteE
 
 // Affichage du résultats en fonction des cas
   if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'complotistes') {
-    result = "Bravo \! Tu as fais gagn\351 le nouvel ordre mondial";
+    result = `Tu as fais gagné le nouvel ordre mondial avec ${carteToi.perso}`;
   }
   else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'comploteurs') {
-    result = "Wahou \! Tu as tu\351 tes alli\351s";
+    result = `Wahou ! Tu as tué ton ami ${carteEnnemi.perso}`;
   }
   else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'comploteurs'){
-    result = "Yes \! Tu as vaincu l\'\351lite p\351dophile satanique";
+    result = `Yes ! Tu as vaincu l'élite pédophile satanique avec ${carteToi.perso}`;
   }
   else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'complotistes'){
-    result = "Wahou \! Tu es le survivant de ta guilde";
+    result = `${carteEnnemi.perso} a succombé ! Tu es le survivant de ta guilde`;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'comploteurs'){
-    result = "Looser \! Le complot mondial a eu ta peau";
+    result = `Looser ! Le complot mondial a eu ta peau en utilisant ${carteEnnemi.perso}`;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'comploteurs'){
-    result = "Oups \! Trop de complot tue le complot";
+    result = `Oups ${carteEnnemi.perso} t'as assassiné ! Trop de complot tue le complot`;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'complotistes'){
-    result = "Looser \! Tu as perdu contre la guilde de la chloroquine";
+    result = `Looser ! Tu as perdu contre ${carteEnnemi.perso} et sa guilde de la chloroquine`;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'complotistes'){
-    result = "Dommage \! Tu as \351t\351 trahis par tes amis";
+    result = `Dommage ! Tu as été trahis par ton ami ${carteEnnemi.perso}`;
   }
   else{
-    result = "Match nul\: Personne n\'est sorti vivant de ce duel";
+    result = `Match nul: Personne n'est sorti vivant de ce duel`;
   }
 
 // débug
