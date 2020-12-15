0c8888f7ef29141222f73cbbef8f35fe28945291
PodZ <74189166+podzit@users.noreply.github.com>
2020-12-15 19:32:17 +0100

    Create data.js
    deck des cartes

---
+++ assets/data.js
@@ -0,0 +1,141 @@
+// Déclaration des cartes
+export const deck = [
+  {
+    categorie: 'comploteurs',
+    force: 1,
+    nom: 'sibeth', // <- la carte 1.jpeg
+    perso: 'Sibeth Ndiaye' 
+  },
+  {
+    categorie: 'comploteurs',
+    force: 2,
+    nom: 'castex',
+    perso: 'Jean Castex'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 2,
+    nom: 'masque',
+    perso: 'Le masque'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 3,
+    nom: 'bigpharma',
+    perso: 'Big Pharma'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 3,
+    nom: 'soros',
+    perso: 'George Soros'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 4,
+    nom: '5g',
+    perso: 'La 5G'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 4,
+    nom: 'gates',
+    perso: 'Bill Gates'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 4,
+    nom: 'bfm',
+    perso: 'BFM TV'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 5,
+    nom: 'vaccin',
+    perso: 'Le vaccin'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 5,
+    nom: 'illuminatis',
+    perso: 'La société secrête des illuminatis'
+  },
+  {
+    categorie: 'comploteurs',
+    force: 6,
+    nom: 'francmacon',
+    perso: 'La franc-maçonnerie'
+  },
+  {
+    categorie: 'complotistes',
+    force: 1,
+    nom: 'holdup',
+    perso: 'Hold Up'
+  },
+  {
+    categorie: 'complotistes',
+    force: 1,
+    nom: 'cnews',
+    perso: 'CNews'
+  },
+  {
+    categorie: 'complotistes',
+    force: 2,
+    nom: 'trump',
+    perso: 'Donald Trump'
+  },
+  {
+    categorie: 'complotistes',
+    force: 2,
+    nom: 'praud',
+    perso: 'Pascal Praud'
+  },
+  {
+    categorie: 'complotistes',
+    force: 3,
+    nom: 'q',
+    perso: 'Q'
+  },
+  {
+    categorie: 'complotistes',
+    force: 3,
+    nom: 'perronne',
+    perso: 'Christian Perronne'
+  },
+  {
+    categorie: 'complotistes',
+    force: 4,
+    nom: 'krusi',
+    perso: 'Ema Krusi'
+  },
+  {
+    categorie: 'complotistes',
+    force: 4,
+    nom: 'engerer',
+    perso: 'Eve Engerer'
+  },
+  {
+    categorie: 'complotistes',
+    force: 4,
+    nom: 'zemmour',
+    perso: 'Eric Zemmour'
+  },
+  {
+    categorie: 'complotistes',
+    force: 5,
+    nom: 'raoult',
+    perso: 'Didier Raoult'
+  },
+  {
+    categorie: 'complotistes',
+    force: 5,
+    nom: 'hcq',
+    perso: "L'hydroxychloroquine"
+  },
+  {
+    categorie: 'complotistes',
+    force: 6,
+    nom: 'trotta',
+    perso: 'Sylvano Trotta'
+  },
+]
