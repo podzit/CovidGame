4fd5c0cb0e9a55fae4557034954574cd19ca5474
podzit <podz@free.fr>
2020-12-17 19:57:21 +0100

    ajout de l'affichage du score de "Toi" et "Ennemi"

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,6 @@
+var scoreWinToi = 0;
+var scoreWinEnnemi = 0;
+
 // Appel de la fonction jeu lors du clic sur le bouton
 document.getElementById("bouton").onclick = function() {jeu()};
 
@@ -38,30 +41,38 @@ const audioloose = `<audio autoplay><source src="assets/audioloose.ogg" type="au
   if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'complotistes') {
     var result = `${gagne} ${carteToi.perso} ${action} ${carteEnnemi.perso}`;
     audio.innerHTML = `${audiowin}`;
+    scoreWinToi ++;
   }
   else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'comploteurs') {
     result = `${gagne} Entre comploteurs, ${carteToi.perso} ${action} ${carteEnnemi.perso}`;
+    scoreWinToi ++;
   }
   else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'comploteurs'){
     result = `${gagne} Tu as vaincu l'élite pédophile satanique avec ${carteToi.perso}`;
     audio.innerHTML = `${audiowin}`;
+    scoreWinToi ++;
   }
   else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'complotistes'){
     result = `${gagne} ${carteEnnemi.perso} a succombé ! Tu es le survivant de ta guilde`;
+    scoreWinToi ++;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'comploteurs'){
     result = `${perd} Le complot mondial t'${action} en utilisant ${carteEnnemi.perso}`;
     audio.innerHTML = `${audioloose}`;
+    scoreWinEnnemi ++;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'comploteurs'){
     result = `${perd} ${carteEnnemi.perso} t'${action}! Trop de complot tue le complot`;
+    scoreWinEnnemi ++;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'complotistes'){
     result = `${perd} ${carteEnnemi.perso} ${action} ${carteToi.perso}`;
     audio.innerHTML = `${audioloose}`;
+    scoreWinEnnemi ++;
   }
   else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'complotistes'){
     result = `${perd} Entre complotistes, ${carteEnnemi.perso} ${action} ${carteToi.perso}`;
+    scoreWinEnnemi ++;
   }
   else{
     result = `Match nul: Personne n'est sorti vivant de ce duel`;
@@ -73,5 +84,9 @@ const audioloose = `<audio autoplay><source src="assets/audioloose.ogg" type="au
 // Affichage des résultats
   
   document.getElementById("resultat").innerHTML = `${result}`;
+
+  // Affichage des scores
+document.getElementById("scoreWinToi").innerHTML = `Score: ${scoreWinToi}`;
+document.getElementById("scoreWinEnnemi").innerHTML = `Score: ${scoreWinEnnemi}`;
 
 }
