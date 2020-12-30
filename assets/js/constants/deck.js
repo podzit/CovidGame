5141a1e523d87c7060cd824b73d2c75914015da6
podzit <podz@free.fr>
2020-12-30 20:56:00 +0100

    création des cartes à la volée

--- assets/js/constants/deck.js
+++ assets/js/constants/deck.js
@@ -1,219 +1,327 @@
 // Déclaration des cartes
 export const deck = [
   {
-    categorie: 'comploteurs', // <- également nom du dossier où se trouve l'image
+    categorie: 'Comploteurs',
+    groupe: 'Inutile',
     force: 1,
     nom: 'sibeth', // <- la carte sibeth.jpeg
-    perso: 'Sibeth Ndiaye' // <- Le nom qui s'affiche dans la phrase de résultat
+    perso: 'Sibeth Ndiaye', // <- Le nom qui s'affiche dans la phrase de résultat
+    info:`Ne sait pas mettre un masque mais y en avait pas donc c'est pas grave.`, // <- info principale du perso
+    effet:`Sème le doute.` // <- Effet du perso qui s'affiche après un saut de ligne
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Tueur',
     force: 1,
     nom: 'linky',
-    perso: 'Linky' 
+    perso: 'Linky',
+    info:`Compteur jaune intelligent communiquant.`,
+    effet:`Sous ses airs inoffensifs envoie du courant dans le cerveau de ses adversaires.` 
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Inutile',
     force: 2,
     nom: 'castex',
-    perso: 'Jean Castex'
+    perso: 'Jean Castex',
+    info:`Monsieur déconfinement. Puis reconfinement. Puis re-déconfinement…`,
+    effet:`Répand confusion sur l’adversaire.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Tueur',
     force: 2,
     nom: 'masque',
-    perso: 'Le masque'
+    perso: 'Le masque',
+    info:`Protège de la mauvaise haleine (et des virus accessoirement).`,
+    effet:`Asphyxie les sachants.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Inutile',
     force: 2,
     nom: 'mouton',
-    perso: 'Le troupeau de moutons'
+    perso: 'Le troupeau de moutons',
+    info:`Les fameux moutons dirigés par les médias. En troupeau vont tous dans la même direction.`,
+    effet:`Gare aux coups de sabots ils sont nombreux.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Lobby',
     force: 3,
     nom: 'bigpharma',
-    perso: 'Big Pharma'
+    perso: 'Big Pharma',
+    info:`Compte ses sous et ce n'est pas suffisant, vraiment pas.`,
+    effet:`Son argent fait interdire les remèdes miracles.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Milliardaire',
     force: 3,
     nom: 'soros',
-    perso: 'George Soros'
+    perso: 'George Soros',
+    info:`Le big boss du complot. 153 ans.`,
+    effet:`Il manipule son adversaire mieux qu’un marionnettiste.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Tueur',
     force: 3,
     nom: 'pcr',
-    perso: 'Le test PCR'
+    perso: 'Le test PCR',
+    info:`Pas très fiable mais trop utilisé quand même.`,
+    effet:`Perfore le cerveau à travers le nez.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Tueur',
     force: 4,
     nom: '5g',
-    perso: 'La 5G'
+    perso: 'La 5G',
+    info:`Propage des ondes partout.`,
+    effet: `Rend les adversaires hypersensibles, sauf ceux vaccinés.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Milliardaire',
     force: 4,
     nom: 'gates',
-    perso: 'Bill Gates'
+    perso: 'Bill Gates',
+    info:`Apparaît dans tous les complots, son nom est devenu une marque.`,
+    effet: `Réduit le nombre d’adversaires à l’aide de vaccins.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Média',
     force: 4,
     nom: 'bfm',
-    perso: 'BFM TV'
+    perso: 'BFM TV',
+    info:`Bergerie Française pour Moutons. 24/7 de propagande des multi-millionnaires à la solde du gouvernement ou l’inverse.`,
+    effet:`Répand le buzz qui rend sourd l’adversaire.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Tueur',
     force: 5,
     nom: 'vaccin',
-    perso: 'Le vaccin'
+    perso: 'Le vaccin',
+    info:`Tue les antivax, épargne les moutons et transforme l'ADN.`,
+    effet:`Régulateur de population mondiale.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Société secrète',
     force: 5,
     nom: 'illuminatis',
-    perso: 'La société secrête des illuminatis'
+    perso: 'La société des illuminatis',
+    info:`Imbattables, ils dirigent tout depuis la nuit des temps. Si c’est pas eux, c’est quand même eux.`,
+    effet:`Tuent les adversaires avant leur naissance.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Religieux',
     force: 5,
     nom: 'satan',
-    perso: 'Satan'
+    perso: 'Satan',
+    info:`Prépare les pires complots grâce aux sacrifices d’enfants.`,
+    effet:`Les flammes de l’enfer brûleront l’adversaire.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Société secrète',
     force: 6,
     nom: 'francmacon',
-    perso: 'La franc-maçonnerie'
+    perso: 'La franc-maçonnerie',
+    info:`Il ne construisent rien malgré leur nom.`,
+    effet:`Par contre ils détruisent les adversaires à coup de truelle.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Société secrète',
     force: 6,
     nom: 'deepstate',
-    perso: `L'état profond`
+    perso: `L'état profond`,
+    info:`Tellement profond qu’on ne l’a jamais vu.`,
+    effet:`Lance des pédophiles sur ses adversaires.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Joker',
     force: 10,
     nom: 'toux',
-    perso: `La quinte de toux`
+    perso: `La quinte de toux`,
+    info:`Toux sèche qui donne envie de se graisser la gorge avec de l'huile de vidange.`,
+    effet:`Disperse des postillons contaminés dans l'air.`
   },
   {
-    categorie: 'comploteurs',
+    categorie: 'Comploteurs',
+    groupe: 'Joker',
     force: 10,
     nom: 'vague',
-    perso: `La 2ème vague`
+    perso: `La 2ème vague`,
+    info:`Personne ne l’avait vu venir. Enfin si mais pas sur les chaines Youtube obscures.`,
+    effet:`Fais chavirer les âmes des adversaires.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Média',
     force: 1,
     nom: 'holdup',
-    perso: 'Hold Up'
+    perso: 'Hold Up',
+    info:`Ouvre les yeux des non moutons.`,
+    effet:`Propage la maladie "crédulité".`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Média',
     force: 1,
     nom: 'cnews',
-    perso: 'CNews'
+    perso: 'CNews',
+    info:`Canal privilégié pour relayer les complots.`,
+    effet:`Jette des intox sur son adversaire pendant qu’il s’enfuie.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Milliardaire',
     force: 2,
     nom: 'trump',
-    perso: 'Donald Trump'
+    perso: 'Donald Trump',
+    info:`Agent orange infiltré contre le nouvel ordre mondial.`,
+    effet:`S'accroche à sa cible jusqu'à la mort.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Patron de PMU',
     force: 2,
     nom: 'praud',
-    perso: 'Pascal Praud'
+    perso: 'Pascal Praud',
+    info:`On ne peut plus rien dire mais lui y arrive.`,
+    effet:`Crie tout ce qui lui passe par la tête plus fort que l’adversaire.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Média',
     force: 2,
     nom: 'sudradio',
-    perso: 'Sud Radio'
+    perso: 'Sud Radio',
+    info:`Radio écoutée uniquement par Eric Zemmour et le RN.`,
+    effet:`Lance des phrases choc. Quel genre ? Genre des phrases choc.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Lanceur d'alerte`,
     force: 3,
     nom: 'q',
-    perso: 'Q'
+    perso: 'Q',
+    info:`Lanceur d'alerte anonyme sans preuve de l'alerte à lancer.`,
+    effet:`Répand la sectite aigüe.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Prophète',
     force: 3,
     nom: 'perronne',
-    perso: 'Christian Perronne'
+    perso: 'Christian Perronne',
+    info:`Le lyme il le déteste, il le préfère dans son verre.`,
+    effet:`Frappe les journalistes sur les plateaux TV armé de ses livres.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: 'Patron de PMU',
     force: 3,
     nom: 'bigard',
-    perso: 'Jean-Marie Bigard'
+    perso: 'Jean-Marie Bigard',
+    info:`Connu pour ses sketches, il en est devenu un.`,
+    effet:`Gueule sur ses adversaires jusqu’à explosion de leurs tympans.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Lanceur d'alerte`,
     force: 4,
     nom: 'krusi',
-    perso: 'Ema Krusi'
+    perso: 'Ema Krusi',
+    info:`Ne connait pas la neutralité Suisse. Elle lui dit merde.`,
+    effet:`Détruit les lois pro-masque.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Lanceur d'alerte`,
     force: 4,
     nom: 'engerer',
-    perso: 'Eve Engerer'
+    perso: 'Eve Engerer',
+    info:`Elle a tout compris ! TOUT ! C’est compris ?`,
+    effet:`Voit au travers de l’adversaire grâce à ses dons de voyance.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Patron de PMU`,
     force: 4,
     nom: 'zemmour',
-    perso: 'Eric Zemmour'
+    perso: 'Eric Zemmour',
+    info:`Pied noir à la parole libre contre les juifs et l’islam.`,
+    effet:`Envoie des faux chiffres sur les conséquences de l’immigration pour effrayer son adversaire.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Prophète`,
     force: 5,
     nom: 'raoult',
-    perso: 'Didier Raoult'
+    perso: 'Didier Raoult',
+    info:`Le covid 19 c'est du fantasme, il n'y croit pas.`,
+    effet:`Ses prophéties sont fausses mais redoutables.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Tueur`,
     force: 5,
     nom: 'hcq',
-    perso: "L'hydroxychloroquine"
+    perso: "L'hydroxychloroquine",
+    info:`Utilisé dans des pays où il est interdit.`,
+    effet:`Soigne les covid+ en bonne santé.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Patron de PMU`,
     force: 5,
     nom: 'soral',
-    perso: "Alain Soral"
+    perso: "Alain Soral",
+    info:`Youtubeur musclé sur canapé.`,
+    effet:`Fais apparaître des leurres juifs pour étourdir l’adversaire.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Prophète`,
     force: 6,
     nom: 'trotta',
-    perso: 'Sylvano Trotta'
+    perso: 'Sylvano Trotta',
+    info:`Star lunaire sur Youtube.`,
+    effet:`Avec lui tout est creux même ses théories.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Prophète`,
     force: 6,
     nom: 'dieudonne',
-    perso: 'Dieudonné'
+    perso: 'Dieudonné',
+    info:`Humoriste censuré reconverti en fabricant de quenelles.`,
+    effet:`Fais des incantations vaudous pour transformer l’adversaire en Jean-Marie Le Pen.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Joker`,
     force: 10,
     nom: 'youtube',
-    perso: 'Youtube'
+    perso: 'Youtube',
+    info:`Si c’est sur Youtube c’est forcément vrai, renseigne-toi !`,
+    effet:`Propage la folie sur ses adversaires.`
   },
   {
-    categorie: 'complotistes',
+    categorie: 'Complotistes',
+    groupe: `Joker`,
     force: 10,
     nom: 'lancetgate',
-    perso: 'Le lancetgate'
+    perso: 'Le lancetgate',
+    info:`Les études scientifiques sont corrompues par les labos à la solde de la CIA.`,
+    effet:`Jette un froid glacial.`
   },
 ]
--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,72 @@ function jeu() {
   // Tirage des cartes
   let carteToi = deck[Math.floor(Math.random() * deck.length)]
   let carteEnnemi = deck[Math.floor(Math.random() * deck.length)];
-
+  
   // Affichage des cartes
-  document.getElementById("img-toi").innerHTML = `<img src="assets/img/${carteToi.categorie}/${carteToi.nom}.jpeg" />`;
-  document.getElementById("img-ennemi").innerHTML = `<img src="assets/img/${carteEnnemi.categorie}/${carteEnnemi.nom}.jpeg" />`;
+  const affcarteToi = `
+  <table class="imagecarte">
+    <tr class="imagecarte">
+      <td class="imagecarte">
+        <table class="carte">
+          <tr class="content">
+            <tr class="top">
+              <td class="top"></td>
+            </tr>
+            <tr class="bandeau">
+              <td class="nom">${carteToi.perso}</td>
+              <td class="force">Force ${carteToi.force}</td>
+            </tr>
+            <tr class="body">
+              <td class="image" colspan="2" style="background-image:url(assets/img/imgcarte/${carteToi.nom}.jpeg)"></td>
+            </tr>
+            <tr class="bandeau">
+              <td class="cat">${carteToi.categorie} / ${carteToi.groupe}</td>
+            </tr>
+            <tr class="info">
+              <td class="info" colspan="2">${carteToi.info}<br/><br/>${carteToi.effet}</td>
+            </tr>
+            <tr class="bottom">
+              <td class="bottom"></td>
+            </tr>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>`;
+
+  const affcarteEnnemi = `
+  <table class="imagecarte">
+    <tr class="imagecarte">
+      <td class="imagecarte">
+        <table class="carte">
+          <tr class="content">
+            <tr class="top">
+              <td class="top"></td>
+            </tr>
+            <tr class="bandeau">
+              <td class="nom">${carteEnnemi.perso}</td>
+              <td class="force">Force ${carteEnnemi.force}</td>
+            </tr>
+            <tr class="body">
+              <td class="image" colspan="2" style="background-image:url(assets/img/imgcarte/${carteEnnemi.nom}.jpeg)"></td>
+            </tr>
+            <tr class="bandeau">
+              <td class="cat">${carteEnnemi.categorie} / ${carteEnnemi.groupe}</td>
+            </tr>
+            <tr class="info">
+              <td class="info" colspan="2">${carteEnnemi.info}<br/><br/>${carteEnnemi.effet}</td>
+            </tr>
+            <tr class="bottom">
+              <td class="bottom"></td>
+            </tr>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>`;
+
+  document.getElementById("img-toi").innerHTML = `${affcarteToi}`;
+  document.getElementById("img-ennemi").innerHTML = `${affcarteEnnemi}`;
 
   let gagne = phrase.motVainqueur[Math.floor(Math.random() * phrase.motVainqueur.length)];
   let perd = phrase.motPerdant[Math.floor(Math.random() * phrase.motPerdant.length)];
@@ -27,39 +89,39 @@ function jeu() {
   const audioloose = `<audio autoplay><source src="assets/audioloose.ogg" type="audio/ogg"></audio>`;
 
   // Affichage du résultats en fonction des cas
-  if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'complotistes') {
+  if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'Comploteurs' && carteEnnemi.categorie == 'Complotistes') {
     var result = `${gagne} ${carteToi.perso} ${action} ${carteEnnemi.perso}`;
     audio.innerHTML = `${audiowin}`;
     scoreWinToi ++;
   }
-  else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'comploteurs') {
+  else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'Comploteurs' && carteEnnemi.categorie == 'Comploteurs') {
     result = `${gagne} Entre comploteurs, ${carteToi.perso} ${action} ${carteEnnemi.perso}`;
     scoreWinToi ++;
   }
-  else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'comploteurs'){
+  else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'Complotistes' && carteEnnemi.categorie == 'Comploteurs'){
     result = `${gagne} Tu as vaincu l'élite pédophile satanique avec ${carteToi.perso}`;
     audio.innerHTML = `${audiowin}`;
     scoreWinToi ++;
   }
-  else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'complotistes'){
+  else if (carteToi.force > carteEnnemi.force && carteToi.categorie == 'Complotistes' && carteEnnemi.categorie == 'Complotistes'){
     result = `${gagne} ${carteEnnemi.perso} a succombé ! Tu es le survivant de ta guilde`;
     scoreWinToi ++;
   }
-  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'comploteurs'){
+  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'Complotistes' && carteEnnemi.categorie == 'Comploteurs'){
     result = `${perd} Le complot mondial t'${action} en utilisant ${carteEnnemi.perso}`;
     audio.innerHTML = `${audioloose}`;
     scoreWinEnnemi ++;
   }
-  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'comploteurs'){
+  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'Comploteurs' && carteEnnemi.categorie == 'Comploteurs'){
     result = `${perd} ${carteEnnemi.perso} t'${action}! Trop de complot tue le complot`;
     scoreWinEnnemi ++;
   }
-  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'comploteurs' && carteEnnemi.categorie == 'complotistes'){
+  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'Comploteurs' && carteEnnemi.categorie == 'Complotistes'){
     result = `${perd} ${carteEnnemi.perso} ${action} ${carteToi.perso}`;
     audio.innerHTML = `${audioloose}`;
     scoreWinEnnemi ++;
   }
-  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'complotistes' && carteEnnemi.categorie == 'complotistes'){
+  else if (carteToi.force < carteEnnemi.force && carteToi.categorie == 'Complotistes' && carteEnnemi.categorie == 'Complotistes'){
     result = `${perd} Entre complotistes, ${carteEnnemi.perso} ${action} ${carteToi.perso}`;
     scoreWinEnnemi ++;
   }
