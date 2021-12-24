085e34718c22e1a184500df7875facbee30021fa
PodZ <74189166+podzit@users.noreply.github.com>
2021-12-24 16:24:06 +0100

    Ajout du bouton "Tapis"

--- assets/js/constants/game.js
+++ assets/js/constants/game.js
@@ -1,4 +1,4 @@
-import { DOM_stop, bets, buttons } from '../constants/display.js';
+import { DOM_betAllin, DOM_stop, bets, buttons } from '../constants/display.js';
 import { reset } from '../main.js';
 import { text } from '../constants/text.js';
 
@@ -12,62 +12,62 @@ export const DOM_formRecord = document.getElementById("formRecord");
 
 // Set environment to start the game
 export function gameReady(){
-    jQuery('#game').show();
-    DOM_pocket.style.display ="block";
-    DOM_pocket.innerHTML = `${text.pocket}50$`;
-    jQuery('#scoresPopForm').hide();
-    jQuery('#replay').hide();
-    jQuery('#resultPopup').hide();
-    bets("inline-block");
-    buttons("auto","#4CAF50");
-    DOM_stop.style.display = "none";
-    DOM_playerImage.innerHTML = DOM_ennemyImage.innerHTML = `<img src="assets/img/back.png"/>`;
-  };
+  jQuery('#game').show();
+  DOM_pocket.style.display ="block";
+  DOM_pocket.innerHTML = `${text.pocket}50$`;
+  jQuery('#scoresPopForm').hide();
+  jQuery('#replay').hide();
+  jQuery('#resultPopup').hide();
+  bets("inline-block");
+  buttons("auto","#4CAF50");
+  DOM_stop.style.display = "none";
+  DOM_betAllin.style.display = "none";
+  DOM_playerImage.innerHTML = DOM_ennemyImage.innerHTML = `<img src="assets/img/back.png"/>`;
+};
 
 // Set environment when game is over
 export function gameOver(){
-    jQuery('#resultPopup').stop(true, true);
-    jQuery('#resultPopup').show();
-    DOM_gain.innerHTML = `${text.replay}`;
-    jQuery('#replay').show();
-    jQuery('#replay').on("click", function () { reset(); } );
-  };
+  jQuery('#resultPopup').stop(true, true);
+  jQuery('#resultPopup').show();
+  DOM_gain.innerHTML = `${text.replay}`;
+  jQuery('#replay').show();
+  jQuery('#replay').on("click", function () { reset(); } );
+};
 
 // Delete game part
 export function noGame() {
   jQuery('#game').hide();
   jQuery('#stop').hide();
+  jQuery('#allin').hide();
   bets("none");
   DOM_pocket.style.display ="none";
-  };
+};
 
 // Display cards
 export function cardDisplay({ guild, group, force, fileName, character, info, effect }, DOM_player) {
   
-    // Delete old card
-    DOM_player.innerHTML = "";
+  // Delete old card
+  DOM_player.innerHTML = "";
   
-    // Copy DOM node of card template
-    const DOM_card = document.getElementById("card").cloneNode(true);
-    DOM_card.id = `card-${DOM_player.id}`;
-    DOM_card.style.display = "block"; // change style to enable display (display: none; in css)
+  // Copy DOM node of card template
+  const DOM_card = document.getElementById("card").cloneNode(true);
+  DOM_card.id = `card-${DOM_player.id}`;
+  DOM_card.style.display = "block"; // change style to enable display (display: none; in css)
+    
+  // Bring values to card template in HTML
+  DOM_card.querySelector('.character').innerHTML = character;
+  DOM_card.querySelector('.force').innerHTML = force;
+  DOM_card.querySelector('.image').style.backgroundImage = `url('assets/img/card/${fileName}.jpeg')`;
+  DOM_card.querySelector('.band').innerHTML = `${guild} / ${group}`;
+  DOM_card.querySelector('.info').innerHTML = info;
+  DOM_card.querySelector('.effect').innerHTML = effect;
   
+  // Add class to force to set color
+  DOM_card.querySelector('.force').classList.add(`force-${force}`);
   
-  
-    // Bring values to card template in HTML
-    DOM_card.querySelector('.character').innerHTML = character;
-    DOM_card.querySelector('.force').innerHTML = force;
-    DOM_card.querySelector('.image').style.backgroundImage = `url('assets/img/card/${fileName}.jpeg')`;
-    DOM_card.querySelector('.band').innerHTML = `${guild} / ${group}`;
-    DOM_card.querySelector('.info').innerHTML = info;
-    DOM_card.querySelector('.effect').innerHTML = effect;
-  
-    // Add class to force to set color
-    DOM_card.querySelector('.force').classList.add(`force-${force}`);
-  
-    // Fill player's DOM with new card
-    DOM_player.appendChild(DOM_card);
-  }
+  // Fill player's DOM with new card
+  DOM_player.appendChild(DOM_card);
+}
   
 // Flip effect
 export function flipCards() {
